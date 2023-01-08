287bb06f0162abaaebf7c7807cb62768dd8094f8
kurupeku <22340645+kurupeku@users.noreply.github.com>
2023-01-09 00:56:41 +0900

    create storage

---
+++ logtransfer/storage/buffer.go
@@ -0,0 +1,47 @@
+package storage
+
+import (
+	"bytes"
+	"context"
+	"io/ioutil"
+	"time"
+)
+
+var (
+	buf = bytes.NewBuffer([]byte{})
+)
+
+func Listen(ctx context.Context, ln chan []byte, errc chan error) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case b, ok := <-ln:
+			if !ok {
+				return
+			}
+			_, err := buf.Write(b)
+			if err != nil {
+				errc <- err
+			}
+		}
+	}
+}
+
+func Load(ctx context.Context, out chan []byte, errc chan error, span time.Duration) {
+	tick := time.NewTicker(span)
+	for {
+		select {
+		case <-ctx.Done():
+			close(out)
+			return
+		case <-tick.C:
+			b, err := ioutil.ReadAll(buf)
+			if err != nil {
+				errc <- err
+				continue
+			}
+			out <- b
+		}
+	}
+}
