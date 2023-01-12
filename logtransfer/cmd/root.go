776acff94dd0be1a3dce1021a967e2372ea70b35
kurupeku <22340645+kurupeku@users.noreply.github.com>
2023-01-12 21:30:37 +0900

    remove comments

--- logtransfer/cmd/root.go
+++ logtransfer/cmd/root.go
@@ -19,7 +19,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "logtransfer",
 	Short: "A log transfer application over HTTP#POST",
@@ -27,7 +26,10 @@ var rootCmd = &cobra.Command{
 The application consists of a distributed system
 with multi-threaded safe transfers.
 
-e.g ) logtransfer https://sample.com sample.sh`,
+- Args1 : request url
+- Args2~: executable command
+
+e.g ) logtransfer https://sample.com sh ./sample.sh`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 2 {
 			return errors.New("request url and command are required")
@@ -38,17 +40,17 @@ e.g ) logtransfer https://sample.com sample.sh`,
 			return err
 		}
 
-		var (
-			ctx, stop     = signal.NotifyContext(context.Background(), os.Interrupt)
-			ln, out, errc = make(chan []byte, 1), make(chan []byte, 1), make(chan error, 1)
-		)
-		defer stop()
-
 		subCmd := exec.Command(args[1], args[2:]...)
 		stdout, err := subCmd.StdoutPipe()
 		if err != nil {
 			return err
 		}
+
+		var (
+			ctx, stop     = signal.NotifyContext(context.Background(), os.Interrupt)
+			ln, out, errc = make(chan []byte, 1), make(chan []byte, 1), make(chan error, 1)
+		)
+		defer stop()
 
 		go input.Monitor(ctx, ln, errc, stdout)
 		go storage.Listen(ctx, ln, errc)
@@ -61,8 +63,6 @@ e.g ) logtransfer https://sample.com sample.sh`,
 	},
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
@@ -70,14 +70,4 @@ func Execute() {
 	}
 }
 
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.logtransfer.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
+func init() {}
