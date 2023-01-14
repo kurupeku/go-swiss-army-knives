94540313a512d40187a60bdc78259d30cb603ad0
kurupeku <22340645+kurupeku@users.noreply.github.com>
2023-01-14 21:52:10 +0900

    craete root cmd

--- cgrep/cmd/root.go
+++ cgrep/cmd/root.go
@@ -5,31 +5,56 @@ package cmd
 
 import (
 	"cgrep/result"
+	"cgrep/search"
 	"os"
 	"path/filepath"
+	"regexp"
+	"sync"
 
 	"github.com/spf13/cobra"
 )
 
 var dir string
+var withContent bool
 
 var rootCmd = &cobra.Command{
-	Use:   "cgrep",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "cgrep [flags] [args]",
+	Short: "Search for file names containing a argument",
+	Long: `Search file names contains argument.
+Arguments are treated as regular expressions.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+Args:
+  A search string that can be compiled as a regular expression`,
+	Args: cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		_, err := filepath.Abs(dir)
+		fullPath, err := filepath.Abs(dir)
 		if err != nil {
 			return err
 		}
 
+		re, err := regexp.Compile(args[0])
+		if err != nil {
+			return err
+		}
+
+		s, err := search.New(fullPath, re)
+		if err != nil {
+			return err
+		}
+
+		var wg = new(sync.WaitGroup)
+		wg.Add(1)
+		go s.Search(wg)
+		wg.Wait()
+
 		if err := result.Error(); err != nil {
 			return err
+		}
+
+		if withContent {
+			result.RenderWithContent()
+		} else {
+			result.RenderFiles()
 		}
 
 		return nil
@@ -44,5 +69,6 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().StringVarP(&dir, "dir", "d", "./", "root directory for searching")
+	rootCmd.Flags().StringVarP(&dir, "dir", "d", "./", "searching directory")
+	rootCmd.Flags().BoolVarP(&withContent, "with-content", "c", false, "render with matched content lines")
 }
