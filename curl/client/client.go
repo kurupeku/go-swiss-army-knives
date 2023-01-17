76a0866593178c386ba75edcd4f7c882b052c71a
gumamon <48235604+gumamon@users.noreply.github.com>
2023-01-17 18:10:52 +0900

    client 作業中

--- curl/client/client.go
+++ curl/client/client.go
@@ -5,8 +5,10 @@ import (
 	"net/http"
 	"net/url"
 	"sort"
+	"strings"
 )
 
+// structは funcの外に書ける。つまりfunc間の値受け渡しに使える
 type HttpClient struct {
 	url           *url.URL
 	method        string
@@ -40,6 +42,11 @@ func NewHttpClient(
 	}
 
 	var rBody *string
+	rHeader, err := setHeader(method, customHeaders)
+	if err != nil {
+		return nil, fmt.Errorf("[ERROR] Can't parse url: %s", rawurl)
+	}
+
 	// method 判別
 	switch method {
 	case "GET", "DELETE":
@@ -53,15 +60,43 @@ func NewHttpClient(
 	}
 
 	client := HttpClient{
-		url:         url,
-		method:      method,
-		requestBody: rBody,
-		//requestHeader: rHeader, #TODO
+		url:           url,
+		method:        method,
+		requestBody:   rBody,
+		requestHeader: rHeader,
 	}
 
 	// HttpClientのポインターで戻り値を返す ,
 	// errorはnil(正常終了)で戻り値を返す
 	return &client, nil
+}
+
+func setHeader(method string, customHeaders []string) (map[string]string, error) {
+	m := make(map[string]string)
+	for _, l := range customHeaders {
+		p := strings.Split(l, ":")
+		// mapにkey , valueを追加。valueはSpaceを除去
+		m[p[0]] = strings.TrimSpace(p[1])
+	}
+
+	switch method {
+	case "GET", "DELETE":
+		// "Content-Type" があったら消す
+		_, ok := m["Content-Type"]
+		if ok {
+			delete(m, "Content-Type")
+		}
+	case "POST", "PUT", "PATCH":
+		// "Content-Type" があったら valueを "application/json" に変更
+		_, ok := m["Content-Type"]
+		if ok {
+			m["Content-Type"] = "application/json"
+		}
+	default:
+		return nil, fmt.Errorf("[ERROR] Unauthorized method: %s", method)
+	}
+
+	return m, nil
 }
 
 func (c *HttpClient) Execute() (string, string, error) {
