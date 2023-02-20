7730d73f5009a37c84799bb148a2d458b3c41efc
Yoshinori Ohminato <yoshinori.ominato@rakus.co.jp>
2023-02-20 09:59:17 +0900

    レンダーOK

--- cgrep/result/result.go
+++ cgrep/result/result.go
@@ -1,8 +1,11 @@
 package result
 
 import (
+	"bytes"
 	"io"
 	"sort"
+	"strconv"
+	"strings"
 	"sync"
 )
 
@@ -34,7 +37,13 @@ func Set(fileName, txt string, no int) {
 // TODO: ファイル名は昇順で出力する
 // TODO: 標準出力は引数 w io.Writer として渡される想定
 func RenderFiles(w io.Writer) {
+
 	// TODO: 2 週目：検索結果のレンダリング & コマンド実行時のメイン処理の実装
+	/* 最終的な標準出力は []byte で渡すため、最初からbyteで処理。
+	   配列文字列の単純な連結は join の方が早い？ */
+	
+	w.Write([]byte(strings.Join(Store.Files(), "\n")))
+	w.Write([]byte("\n"))
 }
 
 // TODO: ファイル名と一致した行番号、一致した行の標準出力に出力する
@@ -43,6 +52,19 @@ func RenderFiles(w io.Writer) {
 // TODO: 標準出力は引数 w io.Writer として渡される想定
 func RenderWithContent(w io.Writer) {
 	// TODO: 2 週目：検索結果のレンダリング & コマンド実行時のメイン処理の実装
+
+	byteTmp := &bytes.Buffer{}
+	for _, fileName := range Store.Files() {
+		byteTmp.Write([]byte(fileName))
+		for _, m := range Store.Data[fileName] {
+			byteTmp.WriteString("\n")
+			byteTmp.WriteString(strconv.Itoa(m.No))
+			byteTmp.WriteString(": ")
+			byteTmp.WriteString(m.Text)
+		}
+		byteTmp.WriteString("\n\n")
+	}
+	w.Write(byteTmp.Bytes()[:len(byteTmp.Bytes())-1]) //普通に作ると、末尾の改行1つが邪魔になるため削除
 }
 
 // 保存されているファイル名を昇順でソートした上で []string として返す関数
