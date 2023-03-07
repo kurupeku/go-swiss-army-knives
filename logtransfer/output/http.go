e2254e6654ae3f9eea75a5be5d59d64242d12ec3
ryotaro-rks <84486303+ryotaro-rks@users.noreply.github.com>
2023-03-07 21:51:26 +0900

    week2

--- logtransfer/output/http.go
+++ logtransfer/output/http.go
@@ -1,7 +1,11 @@
 package output
 
 import (
+	"bytes"
 	"context"
+	"errors"
+	"io"
+	"net/http"
 )
 
 const (
@@ -13,5 +17,32 @@ const (
 // TODO: ctx context.Context がキャンセルされた場合には速やかに関数を終了する
 // TODO: エラーが発生した際には errc chan error へエラーを送信する
 func Forward(ctx context.Context, out chan []byte, errc chan error, url string) {
-	// TODO: 2 週目：内部バッファに保存された内容を一定時間ごとに読み込む処理と、読み取った文字列を Body とした HTTP#POST リクエストを投げる処理
+	for {
+		select {
+		case <-ctx.Done():
+			close(out)
+			return
+		case b, ok := <-out:
+			if !ok {
+				return
+			}
+
+			res, err := http.Post(url, contentType, bytes.NewBuffer(b))
+			if err != nil {
+				errc <- err
+				continue
+			}
+
+			defer res.Body.Close()
+			if res.StatusCode != http.StatusOK {
+				b, err := io.ReadAll(res.Body)
+				if err != nil {
+					errc <- err
+				} else {
+					errc <- errors.New(string(b))
+				}
+			}
+
+		}
+	}
 }
