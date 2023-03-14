5306db8bc7e9eafdf1692f4affd64c98b272077b
gumamon <48235604+gumamon@users.noreply.github.com>
2023-03-14 20:36:27 +0900

    fixed logging

--- logtransfer/cmd/root.go
+++ logtransfer/cmd/root.go
@@ -4,9 +4,15 @@ import (
 	"context"
 	"errors"
 	"io"
+	"logtransfer/input"
+	"logtransfer/logs"
+	"logtransfer/output"
+	"logtransfer/storage"
 	"net/url"
 	"os"
 	"os/exec"
+	"os/signal"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -57,17 +63,35 @@ e.g ) logtransfer https://sample.com sh ./sample.sh`,
 // TODO: context.CancelFunc も同時に返す
 func NewCtx() (context.Context, context.CancelFunc) {
 	// TODO: 3 週目：1 ~ 2 週目の処理を別スレッドで実行しつつ、シグナルを受け取った際にそれらを安全に終了させるメイン処理
-	return nil, nil
+	// https://zenn.dev/nekoshita/articles/dba0a7139854bb#signal.notifycontext%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%9F%E6%9B%B8%E3%81%8D%E6%96%B9
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	return ctx, stop
 }
 
-// TODO: すべての処理を goroutine にて発火させる
-// TODO: 渡す channel のサイズは定数 channelLen を使用して定義する
-// TODO: 各関数に渡す context.Context は引数 ctx context.Context を使用する
-// TODO: 標準出力は r io.Reader として渡される
-// TODO: storage.Load() の実行間隔は定数 timeSpan を利用して渡す
-// TODO: output.Forward() の送信先 URL は引数 u *url.URL を使用して渡す
+// TODO2: 渡す channel のサイズは定数 channelLen を使用して定義する
+// TODO3: 各関数に渡す context.Context は引数 ctx context.Context を使用する
+// TODO4: 標準出力は r io.Reader として渡される
+// TODO5: storage.Load() の実行間隔は定数 timeSpan を利用して渡す
+// TODO6: output.Forward() の送信先 URL は引数 u *url.URL を使用して渡す
 func StartBackgrounds(ctx context.Context, u *url.URL, r io.Reader) {
 	// TODO: 3 週目：1 ~ 2 週目の処理を別スレッドで実行しつつ、シグナルを受け取った際にそれらを安全に終了させるメイン処理
+
+	ln := make(chan []byte)
+	out := make(chan []byte)
+	errc := make(chan error)
+
+	// TODO1: すべての処理を goroutine にて発火させる
+	// TODO: 1 週目：標準出力（`io.Reader` として受け取る）から出力内容を読み取る処理と、読み取った結果を内部のバッファに保存する処理
+	go input.Monitor(ctx, ln, errc, r)
+	// TODO: 1 週目：標準出力（`io.Reader` として受け取る）から出力内容を読み取る処理と、読み取った結果を内部のバッファに保存する処理
+	go storage.Listen(ctx, ln, errc)
+	// TODO: 2 週目：内部バッファに保存された内容を一定時間ごとに読み込む処理と、読み取った文字列を Body とした HTTP#POST リクエストを投げる処理
+	go storage.Load(ctx, out, errc, time.Second*5)
+	// TODO: 2 週目：内部バッファに保存された内容を一定時間ごとに読み込む処理と、読み取った文字列を Body とした HTTP#POST リクエストを投げる処理
+	go output.Forward(ctx, out, errc, u.String())
+
+	go logs.Error(ctx, errc)
+
 }
 
 func Execute() {
--- logtransfer/output/http.go
+++ logtransfer/output/http.go
@@ -1,17 +1,50 @@
 package output
 
 import (
+	"bytes"
 	"context"
+	"errors"
+	"io"
+	"net/http"
 )
 
 const (
 	contentType = "text/plain"
 )
 
-// TODO: 引数 `out chan []byte` で文字列を受信した際に、その内容 Body として引数 `url string` への HTTP#POST リクエストを行う
-// TODO: `Content-Type: plain/text` を Header に添えて送信を行う
-// TODO: ctx context.Context がキャンセルされた場合には速やかに関数を終了する
-// TODO: エラーが発生した際には errc chan error へエラーを送信する
 func Forward(ctx context.Context, out chan []byte, errc chan error, url string) {
 	// TODO: 2 週目：内部バッファに保存された内容を一定時間ごとに読み込む処理と、読み取った文字列を Body とした HTTP#POST リクエストを投げる処理
+	for {
+		select {
+		// TODO3: ctx context.Context がキャンセルされた場合には速やかに関数を終了する
+		case <-ctx.Done():
+			return
+		// TODO1: 引数 `out chan []byte` で文字列を受信した際に
+		case b, ok := <-out:
+			if !ok {
+				return
+			}
+
+			// TODO1: その内容 Body として引数 `url string` への HTTP#POST リクエストを行う
+			body := bytes.NewBuffer(b)
+			// TODO2: `Content-Type: plain/text` を Header に添えて送信を行う
+			res, err := http.Post(url, contentType, body)
+			// TODO4: エラーが発生した際には errc chan error へエラーを送信する
+			if err != nil {
+				errc <- err
+				continue
+			}
+
+			defer res.Body.Close()
+			if res.StatusCode != http.StatusOK {
+				b, err := io.ReadAll(res.Body)
+				// TODO4: エラーが発生した際には errc chan error へエラーを送信する
+				if err != nil {
+					errc <- err
+				} else {
+					errc <- errors.New(string(b))
+				}
+			}
+		}
+	}
 }
